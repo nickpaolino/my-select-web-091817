83db75802aaf66b41e7efc522a24d10518b554f3
github username <github email address>
2017-09-07 10:26:40 -0400

    Finished the my_select method and passed all tests

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
-def my_select(collection)
- # your code here!
+
+def my_select(array)
+ i = 0
+ new_array = []
+ while i < array.length
+   if yield(array[i]) == true
+     new_array << array[i]
+     i += 1
+   else
+     i += 1
+   end
+ end
+ new_array
 end
